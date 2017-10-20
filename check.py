4b6878e34e30aaa60ec4b11b0fa0905af0041bff
Alex Hill <alex.hill@gmail.com>
2017-10-20 13:31:50 +0100

    tidy up

--- check.py
+++ check.py
@@ -10,12 +10,14 @@ import settings
 database_settings = settings.load_settings()
 
 
-def check():
-    tables = get_tables()
-    for table in tables:
-        table_name = table[0]
-        print("Checking table: {table_name}".format(table_name=table_name))
-        check_table(table_name)
+select_primary_key_query = """SELECT Col.Column_Name from 
+    INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, 
+    INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col 
+WHERE 
+    Col.Constraint_Name = Tab.Constraint_Name
+    AND Col.Table_Name = Tab.Table_Name
+    AND Constraint_Type = 'PRIMARY KEY'
+    AND Col.Table_Name = '{table_name}'"""
 
 
 def connect(dbname):
@@ -31,32 +33,11 @@ def connect(dbname):
     return psycopg2.connect(conn_string)
 
 
-def get_tables():
-    tables = None
-    with connect(database_settings["original_db_name"]) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'""")
-            tables = cur.fetchall()
-        cur.close()
-    return tables
-
-
-select_primary_key = """SELECT Col.Column_Name from 
-    INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, 
-    INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col 
-WHERE 
-    Col.Constraint_Name = Tab.Constraint_Name
-    AND Col.Table_Name = Tab.Table_Name
-    AND Constraint_Type = 'PRIMARY KEY'
-    AND Col.Table_Name = '{table_name}'"""
-
-
 def get_table(table_name, db_name):
     rows = None
     with connect(db_name) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-            cur.execute(select_primary_key.format(table_name=table_name))
+            cur.execute(select_primary_key_query.format(table_name=table_name))
             primary_key = cur.fetchall()[0][0]
 
             cur.execute("""SELECT * from {table_name} ORDER BY {primary_key} ASC""".format(table_name=table_name,
@@ -75,6 +56,25 @@ def check_table(table_name):
             # only do this more expensive equality check if the first one fails
             if not utils.ARRAYEQUAL(original_rows[k], new_rows[k]):
                 print("Change in row {rowid}: was {old}, now {new}".format(rowid=k, old=original_rows[k], new=new_rows[k]))
+
+
+def get_tables():
+    tables = None
+    with connect(database_settings["original_db_name"]) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'""")
+            tables = cur.fetchall()
+        cur.close()
+    return tables
+
+
+def check():
+    tables = get_tables()
+    for table in tables:
+        table_name = table[0]
+        print("Checking table: {table_name}".format(table_name=table_name))
+        check_table(table_name)
 
 
 if __name__ == "__main__":
