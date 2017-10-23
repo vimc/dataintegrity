b5f0d0d6567e01f10606c48f7985494901148e61
Martin Eden <martin.s.eden@gmail.com>
2017-10-23 11:45:16 +0100

    Rename function.

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 
-def sql_equal(a, b):
+def equal_or_both_NaN(a, b):
     return a == b or ((a != a) and (b != b))
 
 
 def array_equal(x, y):
     for i in range(0, len(x)):
-        if not sql_equal(x[i], y[i]):
+        if not equal_or_both_NaN(x[i], y[i]):
             print("Not equal: {old}, {new}".format(old=x[i], new=y[i]))
             return False
     return True
